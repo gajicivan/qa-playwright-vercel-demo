e802969d99329bb3bbce6b9dca13665ba5b8b8b2
gajicivan <ivan.joga.gajic@gmail.com>
2026-04-08 11:16:44 +0200

    webserver data removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,5 @@ export default defineConfig({
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     headless: true
-  },
-   webServer: {
-    command: 'npx vercel dev',
-    port: 3000,
-    reuseExistingServer: true,
-    timeout: 120 * 1000, // 2 min
-  },
+  }
 });
